c36565b79863eab4acd4e94f616580f5094ba2fc
Max <sibo_jian_29903549@163.com>
2019-05-08 18:21:20 +0800

    [功能]($JSLockerConstants)
    添加 JSLockerPresentationDirection 枚举，用于区分 Locker 动画方向。

---
+++ Sources/Core/JSLockerConstants.swift
@@ -0,0 +1,15 @@
+//
+//  JSLockerConstants.swift
+//  JSLocker
+//
+//  Created by Max on 2019/5/8.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+import Foundation
+
+// MARK: JSLockerPresentationDirection
+public enum JSLockerPresentationDirection: Int {
+    case down  // locker animated down from a top base
+    case up    // locker animated up from a bottom base
+}
